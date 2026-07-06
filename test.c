e3d5c312e2acc152f45f2c5d394230662c5b5ad2
kvthweatt <kvthweatt@gmail.com>
2026-07-06 09:14:56 -0400

    Experimental C->Flux translation utility.

--- test.c
+++ test.c
@@ -1,8 +1,22 @@
-#include <cstdio>
+#include <stdio.h>
 
-int main()
-{
-	int x = 10;
-	
-	return 0;
+int main() {
+    int rows, i, j;
+    
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    
+    for (i = 1; i <= rows; i++) {
+        // Print spaces for alignment
+        for (j = i; j < rows; j++) {
+            printf(" ");
+        }
+        // Print asterisks
+        for (j = 1; j <= (2 * i - 1); j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
 }
